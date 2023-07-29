b77b001510fa899740be372091ae0d5b1ade43aa
princewritecode <princewritecode@gmail.com>
2023-07-29 23:43:42 +0530

    create read completed successfully

--- index..js
+++ index..js
@@ -1,21 +1,40 @@
 //create express server
-const app = require('express')();
+const express = require("express");
+const app = express();
 const path = require("path");
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "/views"));
+app.use(express.urlencoded({ extended: true }));
+const comments = [
+    { username: "prince", tweet: "hey buddy this is it" },
+    { username: "sunny", tweet: "yes brother we did it" },
+    { username: "suryansh", tweet: "let's make this world a better place" }
+];
+app.get("/tweets", (req, res) => {
+    res.render("tweets/index.ejs", { comments });
+});
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/tweets/new", (req, res) => {
+    res.render("tweets/new.ejs");
+});
+
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body;
+    comments.push({ username, tweet });
+    res.send("it worked");
 });
 
 
-
-
-
-
-
-
-
+/*
+app.get("/tacos", (req, res) => {
+    res.send("get request");
+});
+app.post("/tacos", (req, res) => {
+    const username = req.body.username;
+    const tweet = req.body.tweet;
+    res.send(` worked and the values are ${username} and ${tweet} `);
+});
+*/
 app.listen(80, () => {
     console.log("listening to port number 80");
 });
